9a6b968d0c0c3b336407e06ce83f09f3287faf88
shainamainar <shainamainar@gmail.com>
2019-09-24 16:19:40 -0400

    restart v2

--- Shell HW/shell_v2.c
+++ Shell HW/shell_v2.c
@@ -9,31 +9,3 @@
 #define MAX_CMD 10
 
 
-void shell(){
-  char in[BUFFER_SIZE];
-  char *cmd[MAX_CMD]; 
-  bool isBackground = false;
-  int i = 0;
-  printf(">");
-  while(fgets(in, BUFFER_SIZE, stdin) && strcmp (in, "exit\n") != 0){
-    char *tokens = strtok(in, " ");
-    in[i] = tokens;
-    i += 1;
-    while(tokens != NULL){
-      tokens = strtok (NULL, " ");
-      in[i] = tokens;
-    }
-    if(strcmp(in[i-2], "&") == 0){
-      in[i-2] = NULL;
-      isBackground = true;
-    }
-  }
-  
-}
-
-int main(){
-
-  shell();
-  return EXIT_SUCCESS;
-}
-
