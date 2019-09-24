b37fbd0f83939e1f2ef4352e5529875a8b025f8a
shainamainar <shainamainar@gmail.com>
2019-09-24 17:03:27 -0400

    input implemented

--- Shell HW/shell_v2.c
+++ Shell HW/shell_v2.c
@@ -9,3 +9,33 @@
 #define MAX_CMD 10
 
 
+int main(){
+  char argBuf[BUFFER_SIZE];
+  char* argv[100]; //user arg
+  int argc; //arg count
+    int i = 0;
+
+    printf(">");
+    while(fgets(argBuf, BUFFER_SIZE, stdin)){
+      char *token;
+      token = strtok(argBuf, " ");
+      
+      while(token != NULL && (strcmp(token, "exit\n") != 0)){
+        argv[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+      }
+
+    }
+    argv[i] = NULL;
+    argc = i;
+    for(i = 0; i < argc; i++){
+      printf("%s\n", argv[i]);
+    }
+  int pid = fork();
+  
+  
+ 
+}
+
+
