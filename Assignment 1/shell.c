3eb4693c3a00a3bc6d14a546b7505aca3fa32add
Shaina Mae Mainar <mainarsa@cofc.edu>
2019-09-25 13:15:40 -0400

    add comments

--- Assignment 1/shell.c
+++ Assignment 1/shell.c
@@ -1,3 +1,7 @@
+//
+//  shell.c
+//  Shaina Mae Mainar
+//
 //imported libraries
 #include<stdio.h> 
 #include<string.h> 
@@ -10,49 +14,55 @@
 #define BUFFER_SIZE 32
 #define MAX_CMD 10
 
-//TODO: copy output to file
 
 int main(){
-  char argBuf[BUFFER_SIZE];
+  char argBuf[BUFFER_SIZE]; // initial input
   printf(">");
   bool back = false;
     
   while((fgets(argBuf, BUFFER_SIZE, stdin)) && *argBuf != '\n'){
+    // remove new line character
     argBuf[strcspn(argBuf, "\n")] = 0;
     int i = 0;
     char* argv[10]; //user arg
     if(strcmp(argBuf, "exit") == 0) exit(0);
-    char *token;
+    char *token; //individual tokens
     token = strtok(argBuf, " ");
+	//add tokens to arguments array
     while(token != NULL){
-      //printf("%s\n", token);
       argv[i] = token;
       token = strtok(NULL, " ");
       i++;
     } 
+	//removing new line characters so execvp doesnt break
     if (argBuf[strlen(argBuf)-1] == '\n')
     argBuf[strlen(argBuf)-1] = '\0';
-
+	//background process
     if(strcmp(argv[i-1], "&") == 0){
       argv[--i] = NULL;
       back = true;
     }
-      
+    //fork child
     printf("PID is: %d\n", getpid());
     pid_t pid = fork();
     int status;
+	//fork method fails
     if(pid < 0){
       printf("fork fail\n");
       exit(1);
     }
+	//child executes
     else if(pid == 0){
       execvp(argv[0], argv);
       perror("execvp fail\n");
       exit(0);
     }
+	//parent waits for child
     else{
       while(wait(&status) != pid);
+	  //dont wait for background process
       if(!(back)) wait(&status);
+	  //remove date from arguments array before taking in new input
       memset(argv, 0, BUFFER_SIZE);
       //free(argv);
       printf(">");
